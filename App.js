88d5124b8a5810127455e30ae26f374917a03c45
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-06-28 16:21:11 +0530

    Build NavOptions component

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
@@ -7,7 +8,9 @@ export default function App() {
 	return (
 		// Redux setup
 		<Provider store={store}>
-			<HomeScreen />
+			<SafeAreaProvider>
+				<HomeScreen />
+			</SafeAreaProvider>
 		</Provider>
 	);
 }
