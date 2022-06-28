6849a4a839d16c0fa545c399629959c1b881ef0e
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-06-28 15:33:44 +0530

    HomeScreen Component

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
 
 export default function App() {
 	return (
 		// Redux setup
 		<Provider store={store}>
-			<View style={styles.container}>
-				<Text>Let's build UBER!</Text>
-			</View>
+			<HomeScreen />
 		</Provider>
 	);
 }
