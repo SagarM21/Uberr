488a18c2710c0106f3eb92483d040b4d178a786e
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-06-29 17:12:42 +0530

    Implemented react native navigation successfully

--- App.js
+++ App.js
@@ -3,14 +3,31 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MapScreen from "./screens/MapScreen";
 
 export default function App() {
+	const Stack = createNativeStackNavigator();
 	return (
 		// Redux setup
 		<Provider store={store}>
-			<SafeAreaProvider>
-				<HomeScreen />
-			</SafeAreaProvider>
+			<NavigationContainer>
+				<SafeAreaProvider>
+					<Stack.Navigator>
+						<Stack.Screen
+							name='HomeScreen'
+							component={HomeScreen}
+							options={{ headerShown: false }}
+						/>
+						<Stack.Screen
+							name='MapScreen'
+							component={MapScreen}
+							options={{ headerShown: false }}
+						/>
+					</Stack.Navigator>
+				</SafeAreaProvider>
+			</NavigationContainer>
 		</Provider>
 	);
 }
