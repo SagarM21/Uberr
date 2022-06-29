d97581dd360718aa3b3423c3a150dd908c38b1a1
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-06-29 21:40:57 +0530

    Implemented Google Autocomplete library for searching destinations - HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,14 +2,50 @@ import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 import React from "react";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
+	const dispatch = useDispatch();
 	return (
 		<SafeAreaView style={tw`bg-white h-full`}>
 			<View style={tw`p-5`}>
 				<Image
 					style={{ width: 100, height: 100, resizeMode: "contain" }}
 					source={{ uri: "https://links.papareact.com/gzs" }}
+				/>
+
+				<GooglePlacesAutocomplete
+					nearbyPlacesAPI='GooglePlacesSearch'
+					styles={{
+						container: {
+							flex: 0,
+						},
+						textInput: {
+							fontSize: 18,
+						},
+					}}
+					onPress={(data, details = null) => {
+						dispatch(
+							setOrigin({
+								location: details.geometry.location,
+								description: data.description,
+							})
+						);
+						dispatch(setDestination(null));
+					}}
+					fetchDetails={true}
+					returnKeyType={"search"}
+					query={{
+						key: GOOGLE_MAPS_APIKEY,
+						language: "en",
+					}}
+					minLength={2}
+					enablePoweredByContainer={false}
+					debounce={400}
+					placeholder='Where From?'
 				/>
 
 				<NavOptions />
