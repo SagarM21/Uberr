79e187ff07b4743801ae31b8d534636187e420b4
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-07-01 16:40:13 +0530

    Navigate Card - built half - error api not working

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,14 +1,65 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React from "react";
+import tw from "tailwind-react-native-classnames";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { useDispatch } from "react-redux";
+import { setDestination } from "../slices/navSlice";
+import { useNavigation } from "@react-navigation/native";
 
 const NavigateCard = () => {
-  return (
-    <View>
-      <Text>NavigateCard</Text>
-    </View>
-  )
-}
+	const dispatch = useDispatch();
+	const navigation = useNavigation();
+	return (
+		<View>
+			<SafeAreaView style={tw`flex-1 bg-white`}>
+				<Text style={tw`text-center py-5 text-xl`}>Good Morning, Sagar</Text>
+				<View style={tw`border-t border-gray-200 flex-shrink`}>
+					<View>
+						<GooglePlacesAutocomplete
+							placeholder='Where To?'
+							debounce={400}
+							styles={toInputBoxStyles}
+							enablePoweredByContainer={false}
+							query={{
+								key: GOOGLE_MAPS_APIKEY,
+								language: "en",
+							}}
+							onPress={(data, details = null) => {
+								dispatch(
+									setDestination({
+										location: details.geometry.location,
+										description: data.description,
+									})
+								);
+								navigation.navigate("RideOptionsCard");
+							}}
+							nearbyPlacesAPI='GooglePlacesSearch'
+							returnKeyType={"search"}
+							minLength={2}
+						/>
+					</View>
+				</View>
+			</SafeAreaView>
+		</View>
+	);
+};
 
-export default NavigateCard
+export default NavigateCard;
 
-const styles = StyleSheet.create({})
+const toInputBoxStyles = StyleSheet.create({
+	container: {
+		backgroundColor: "white",
+		paddingTop: 20,
+		flex: 0,
+	},
+	textInput: {
+		backgroundColor: "#DDDDDF",
+		borderRadius: 0,
+		fontSize: 18,
+	},
+	textInputContainer: {
+		paddingHorizontal: 20,
+		paddingBottom: 0,
+	},
+});
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -4,6 +4,7 @@ import tw from "tailwind-react-native-classnames";
 import Map from "../components/Map";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import NavigateCard from "../components/NavigateCard";
+import RideOptionsCard from "../components/RideOptionsCard";
 
 const MapScreen = () => {
 	const Stack = createNativeStackNavigator();
